b823a9c3cd13ad4c8baaaccdaf1e3a26aca844d1
ParbhatKataria <parbhatkataria628@gmail.com>
2023-07-26 08:30:55 +0530

    fix:type errro in card_main

--- src/app/components/card_main.tsx
+++ src/app/components/card_main.tsx
@@ -2,8 +2,14 @@
 import React, { useContext } from 'react';
 import articles_data, { NewsSchema } from '@/utils/fetch';
 import Card from './card';
-import { AuthContext, MyComponentProps } from '@/context/AuthContext';
+import { AuthContext } from '@/context/AuthContext';
 import { useRouter } from 'next/navigation';
+
+
+export interface MyComponentProps {
+  children: React.ReactNode;
+}
+
 
 const CardMain = ({children}:MyComponentProps) => {
     const { user } = useContext(AuthContext)
