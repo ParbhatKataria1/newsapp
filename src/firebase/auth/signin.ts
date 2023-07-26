15083f55d541cd2a930e4ffa2c45a570a5602c69
ParbhatKataria <parbhatkataria628@gmail.com>
2023-07-26 08:34:10 +0530

    fix:type error in auth

--- src/firebase/auth/signin.ts
+++ src/firebase/auth/signin.ts
@@ -4,7 +4,7 @@ import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
 const auth = getAuth(firebase_app);
 
 
-export default async function signIn(email:String, password:String  ) {
+export default async function signIn(email:string, password:string  ) {
     let result = null,
         error = null;
     try {
